7bfb13c401ff1faf5d146b0eb2623306ed4ac659
M_pavan <pavanmangina22@gmail.com>
2025-12-29 17:48:07 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-
+const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -13,6 +13,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api", require("./routes/taskRoutes"))
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000")
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT)
 })
